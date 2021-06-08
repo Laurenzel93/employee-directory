f59bf8a5de0abe19f17dbc44bacce0f5fcda432c
Lauren Wenzel <Laurenzel93@aol.com>
2021-06-08 16:01:41 -0400

    added default React App things

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+   
+  );
+}
+
+export default App;
